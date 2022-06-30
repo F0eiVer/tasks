023b7890933853af5e56fb8862b1f2bc66103e25
F0eiVer <arefev030303@mail.ru>
2022-06-30 15:57:55 +1000

    RLE
    RLE fixed

--- RLE/RLE.cpp
+++ RLE/RLE.cpp
@@ -1,74 +1,70 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#pragma pack(1)
-#define uc unsigned char
+#include <string>
 #define ull unsigned long long
 
 using namespace std;
 
-struct Node{
-    uc help_b;
-    uc value;
-};
-
 int main(){
     ifstream fin("input.txt");
     ofstream fout("output.txt");
-    //input
-    vector<uc> in = {0, 0, 0, 0, 0, 0, 4, 2, 0, 4, 4, 4, 4, 4, 4, 4, 80, 80, 80, 80, 0, 2, 2, 2, 2, 255, 255, 255, 255, 255, 0};
-    vector<Node> arr;
-    vector<uc> ans; // Для декодирования
-    Node tmp_n;
-    uc tmp;
-    tmp_n.help_b = 0;
-    // Сжатие
-    for(ull i=1; i<(ull)in.size(); ++i){
-        tmp = in[i];
-        if(in[i-1] == tmp && tmp_n.help_b == 0){
-            tmp_n.help_b = 128;
-            tmp_n.value = tmp;
-        }
-        else if(in[i-1] == tmp && tmp_n.help_b < 255){
-            tmp_n.help_b++;
-        }
-        else if(tmp_n.help_b == 255){
-            arr.push_back(tmp_n);
-            tmp_n.help_b = 0;
+    string str, ans;
+    ull num = 1;
+    char tmp;
+    fin >> str;
+    ans.push_back(str[0]);
+    tmp = str[0];
+
+    // Кодирование
+    for(ull i=1; i<(ull)str.size(); ++i){
+
+        if(tmp == str[i]){
+            num++;
         }
         else{
-            arr.push_back(tmp_n);
-            tmp_n.value = tmp;
-            tmp_n.help_b = 0;
+            ans = ans + to_string(num);
+            num = 1;
+            ans.push_back(str[i]);
+            tmp = str[i];
         }
     }
-    arr.push_back(tmp_n);
-
-    // Результат
-    for(ull i=0; i<(ull)arr.size(); ++i){
-        if(arr[i].help_b/128)
-            cout << (int)arr[i].help_b%128 + 2 << " " << (int)arr[i].value << "\n";
-        else
-            cout << (int)arr[i].value << "\n";
+    if(num > 1){
+        ans += to_string(num);
     }
-    cout << "\n" << "----------------------------------------" << "\n\n";
+    num = 0;
+    str = string("");
+    // Вывод ответа
+    cout << ans << "\n";
 
-    // Декодирование
-    for(ull i=0; i<(ull)arr.size(); ++i){
-        if(arr[i].help_b/128){
-            for(uc j=0; j<arr[i].help_b%128 + 2; ++j){
-                ans.push_back(arr[i].value);
+
+    //Декодирование
+    for(ull i=0; i<(ull)ans.size(); ++i){
+        if((int)ans[i] < 48 || (int)ans[i] > 57){
+            while(num){
+                str.push_back(tmp);
+                num--;
+            }
+            tmp = ans[i];
+        }
+        else{
+            if(!num)
+                num = (int)ans[i] - 48;
+            else{
+                num *= 10;
+                num += (int)ans[i] - 48;
             }
         }
-        else{
-            ans.push_back(arr[i].value);
+    }
+
+    if(num){
+        while(num){
+            str.push_back(tmp);
+            num--;
         }
     }
 
-    // Вывод ответа
-    for(ull i=0; i<(ull)ans.size(); ++i){
-        cout << (int)ans[i] << " ";
-    }
+    //Изначальные данные
+    cout << str;
 
     fin.close();
     fout.close();
